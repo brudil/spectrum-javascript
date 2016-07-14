c103a7ca4899978c3698558b8ea270e34d9e654f
James Canning <james@jamescanning.com>
2016-07-14 17:47:43 +0100

    😳

--- src/spectrum.ts
+++ src/spectrum.ts
@@ -4,6 +4,6 @@ import * as subtypes from './subtypes';
 import * as fields from './fields';
 import * as sections from './sections';
 import * as transformers from './transformers';
-import * as resources from './transformers';
+import * as resources from './resources';
 
 export {SpectrumDocument, blocks, subtypes, fields, sections, transformers, resources};
